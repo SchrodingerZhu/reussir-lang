cffc62859f33aab51a449dbb9a993d750becc794
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-02-10 00:07:40 +0800

    start new frontend

--- reussir-lang/src/syntax/mod.rs
+++ reussir-lang/src/syntax/mod.rs
@@ -1,74 +1,10 @@
-use std::collections::HashMap;
-use std::sync::atomic::{AtomicU64, Ordering};
+use bumpalo::Bump;
 
-pub mod r#abstract;
-pub mod concrete;
-pub mod surface;
+mod expr;
+mod func;
+mod r#type;
 
-pub type ID = u64;
-
-pub type NameMap<'src> = HashMap<&'src str, ID>;
-
-#[derive(Debug, Copy, Clone)]
-pub struct Ident<'src> {
-    pub raw: &'src str,
-    pub id: ID,
-}
-
-pub fn fresh() -> ID {
-    static NEXT_ID: AtomicU64 = AtomicU64::new(1);
-    NEXT_ID.fetch_add(1, Ordering::Relaxed)
-}
-
-pub trait Syntax {}
-
-#[allow(dead_code)]
-#[derive(Debug, Clone)]
-pub struct Param<'src, T: Syntax> {
-    pub name: Ident<'src>,
-    pub typ: Box<T>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-pub struct FnSig<'src, T: Syntax> {
-    pub typ_params: Box<[Param<'src, T>]>,
-    pub val_params: Box<[Param<'src, T>]>,
-    pub eff: Box<T>,
-    pub ret: Box<T>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-pub struct FnDef<'src, T: Syntax> {
-    pub sig: FnSig<'src, T>,
-    pub body: Box<T>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-pub struct DataDef<'src, T: Syntax> {
-    pub typ_params: Box<[Param<'src, T>]>,
-    pub ctors: Box<[Ctor<'src, T>]>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-pub struct Ctor<'src, T: Syntax> {
-    pub name: Ident<'src>,
-    pub params: CtorParams<'src, T>,
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-pub enum CtorParams<'src, T: Syntax> {
-    None,
-    Unnamed(Box<[Box<T>]>),
-    Named(Box<[Param<'src, T>]>),
-}
-
-impl<T: Syntax> Default for CtorParams<'_, T> {
-    fn default() -> Self {
-        Self::None
-    }
+struct Context {
+    allocator: Bump,
+    input: std::path::PathBuf,
 }
---
+++ reussir-lang/src/syntax/type.rs
@@ -0,0 +1,48 @@
+pub enum Type<'ctx> {
+    Int(Int),
+    Float(Float),
+    Str,
+    Composite(&'ctx [Self]),
+}
+
+pub struct Int {
+    width_ilog: u32,
+    signed: bool,
+}
+
+impl Int {
+    pub fn new(width: usize, signed: bool) -> Self {
+        assert!(
+            width.is_power_of_two(),
+            "width must be a power of 2, found {}.",
+            width
+        );
+        Self {
+            width_ilog: width.ilog2(),
+            signed,
+        }
+    }
+    pub fn width(&self) -> usize {
+        1 << self.width_ilog
+    }
+    pub fn is_signed(&self) -> bool {
+        self.signed
+    }
+}
+
+pub enum Float {
+    F16,
+    BF16,
+    F32,
+    F64,
+    F128,
+}
+
+pub enum FieldName<'ctx> {
+    Idx(usize),
+    Name(&'ctx str),
+}
+
+pub struct Composite<'ctx> {
+    name: &'ctx str,
+}
