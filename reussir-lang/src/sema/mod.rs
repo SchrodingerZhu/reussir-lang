b8237f80db415d5be509e739fda9d31bd0433ace
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-02-25 17:58:20 +0800

    support elaboration in core terms

--- reussir-lang/src/sema/mod.rs
+++ reussir-lang/src/sema/mod.rs
@@ -1,27 +1,35 @@
 #![allow(unused)]
 mod term;
-mod r#type;
 use std::{
     marker::PhantomData,
     ops::{Deref, DerefMut},
 };
 
-use allocator_api2::{boxed::Box, vec::Vec};
+use chumsky::span::SimpleSpan;
 use gc_arena::{allocator_api::MetricsAlloc, lock, Arena, Collect, Gc, Mutation, Rootable, Static};
 use rustc_hash::{FxBuildHasher, FxRandomState};
 use smallvec::SmallVec;
 
-use crate::syntax;
-
+use crate::syntax::{self, WithSpan};
+use term::Term;
 type Ref<'gc, T> = Gc<'gc, T>;
 type RefCell<'gc, T> = Gc<'gc, lock::RefLock<T>>;
 type Cell<'gc, T> = Gc<'gc, lock::Lock<T>>;
 type HashMap<'gc, K, V> = hashbrown::HashMap<K, V, FxRandomState, MetricsAlloc<'gc>>;
+type Vec<'gc, T> = allocator_api2::vec::Vec<T, MetricsAlloc<'gc>>;
+type Box<'gc, T> = allocator_api2::boxed::Box<T, MetricsAlloc<'gc>>;
 type UStr = Static<ustr::Ustr>;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Collect)]
+#[collect(require_static)]
+pub enum FieldName {
+    Idx(usize),
+    Name(UStr),
+}
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Collect)]
 #[collect(no_drop)]
-pub struct QualifiedName<'gc>(Box<[UStr], MetricsAlloc<'gc>>, UStr);
+pub struct QualifiedName<'gc>(Box<'gc, [UStr]>, UStr);
 
 impl<'gc> QualifiedName<'gc> {
     pub fn new<'a>(mutator: &Mutation<'gc>, name: syntax::QualifiedName<'a>) -> Self {
@@ -63,12 +71,6 @@ impl<'gc> Context<'gc> {
 
 type CtxRef<'gc> = Ref<'gc, Context<'gc>>;
 
-#[derive(Clone, Collect)]
-#[collect(no_drop)]
-enum Term<'gc> {
-    Todo(PhantomData<&'gc ()>),
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -81,5 +83,37 @@ mod test {
             let name = QualifiedName::new(mc, fake_name);
         });
         arena.finish_cycle();
+    }
+}
+
+#[derive(Copy, Clone, Collect, Eq, Debug)]
+#[collect(no_drop)]
+#[repr(transparent)]
+pub struct UniqueName<'gc>(Gc<'gc, Static<WithSpan<ustr::Ustr>>>);
+
+impl<'gc> UniqueName<'gc> {
+    fn new<T: Into<ustr::Ustr>>(mc: &Mutation<'gc>, name: T, span: SimpleSpan) -> Self {
+        Self(Gc::new(mc, Static(WithSpan(name.into(), span))))
+    }
+    fn fresh<T: Into<ustr::Ustr>>(mc: &Mutation<'gc>, span: SimpleSpan) -> Self {
+        Self(Gc::new(mc, Static(WithSpan("$x".into(), span))))
+    }
+    fn span(&self) -> SimpleSpan {
+        self.0 .1
+    }
+    fn name(&self) -> ustr::Ustr {
+        *self.0 .0
+    }
+}
+
+impl PartialEq for UniqueName<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        Gc::ptr_eq(self.0, other.0)
+    }
+}
+
+impl std::hash::Hash for UniqueName<'_> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        Gc::as_ptr(self.0).hash(state);
     }
 }
---
+++ reussir-lang/src/sema/term.rs
@@ -0,0 +1,340 @@
+use std::{ops::Deref, rc::Rc};
+
+use super::{FieldName, UniqueName, Vec};
+use archery::RcK;
+use chumsky::span::SimpleSpan;
+use gc_arena::{allocator_api::MetricsAlloc, lock, Arena, Collect, Gc, Mutation, Rootable, Static};
+use rpds::HashTrieMap;
+use rustc_hash::FxRandomState;
+
+use crate::syntax::WithSpan;
+
+use super::{Box, QualifiedName, UStr};
+
+pub type TermPtr<'gc> = Gc<'gc, WithSpan<Term<'gc>>>;
+pub type ValuePtr<'gc> = Gc<'gc, WithSpan<Value<'gc>>>;
+
+#[derive(Clone, Collect)]
+#[collect(no_drop)]
+pub enum Term<'gc> {
+    /// Integer literal
+    Integer(Static<rug::Integer>),
+    /// Float literal
+    Float(Static<rug::Float>),
+    /// String literal
+    Str(UStr),
+    /// Boolean literal
+    Boolean(bool),
+    /// function call
+    FuncCall {
+        target: WithSpan<QualifiedName<'gc>>,
+        ty_args: Box<'gc, [TermPtr<'gc>]>,
+        arguments: Box<'gc, [TermPtr<'gc>]>,
+    },
+    /// constructor call
+    CtorCall {
+        target: QualifiedName<'gc>,
+        ty_args: Box<'gc, [TermPtr<'gc>]>,
+        arguments: Box<'gc, [(FieldName, TermPtr<'gc>)]>,
+    },
+    /// closure call
+    ClosureCall(TermPtr<'gc>, TermPtr<'gc>),
+    /// project a field out of a record
+    Proj {
+        value: TermPtr<'gc>,
+        field: FieldName,
+    },
+    /// match variant
+    Match {},
+    /// cast variant
+    Cast {},
+    /// lift function into a closure
+    FuncAbs {
+        target: QualifiedName<'gc>,
+        ty_args: Box<'gc, [TermPtr<'gc>]>,
+    },
+    /// lift constructor into a closure
+    CtorAbs {
+        target: QualifiedName<'gc>,
+        ty_args: Box<'gc, [TermPtr<'gc>]>,
+    },
+    /// Lambda expression
+    Lambda {
+        binding: Gc<'gc, List<'gc, UniqueName<'gc>>>,
+        body: TermPtr<'gc>,
+    },
+    /// Let binding
+    Let {
+        name: UniqueName<'gc>,
+        binding: TermPtr<'gc>,
+        body: TermPtr<'gc>,
+    },
+    /// Sequence
+    Seq(TermPtr<'gc>, TermPtr<'gc>),
+    /// Builtin Integer types,
+    IntTy(Static<crate::syntax::r#type::Int>),
+    /// Builtin Float types
+    FloatTy(Static<crate::syntax::r#type::Float>),
+    /// Pi type
+    Pi {
+        name: UniqueName<'gc>,
+        arg: TermPtr<'gc>,
+        body: TermPtr<'gc>,
+    },
+    Var(UniqueName<'gc>),
+    StrTy,
+    BooleanTy,
+    Universe,
+    MetaVar(UniqueName<'gc>),
+    CheckVar,
+    Invalid,
+}
+
+type Map<K, V> = HashTrieMap<K, V, RcK, FxRandomState>;
+
+#[derive(Clone)]
+pub struct Closure<'gc> {
+    variables: Map<UniqueName<'gc>, ValuePtr<'gc>>,
+    body: TermPtr<'gc>,
+}
+
+unsafe impl<'gc> Collect<'gc> for Closure<'gc> {
+    const NEEDS_TRACE: bool = true;
+
+    fn trace<T: gc_arena::collect::Trace<'gc>>(&self, cc: &mut T) {
+        for (k, v) in self.variables.iter() {
+            k.trace(cc);
+            v.trace(cc);
+        }
+    }
+}
+
+#[derive(Clone, Collect)]
+#[collect(no_drop)]
+pub enum Value<'gc> {
+    /// cannot elaborate
+    Stuck(TermPtr<'gc>),
+    Var(UniqueName<'gc>),
+    ClosureCall(ValuePtr<'gc>, ValuePtr<'gc>),
+    Universe,
+    Pi {
+        name: UniqueName<'gc>,
+        arg: ValuePtr<'gc>,
+        body: Closure<'gc>,
+    },
+    Lambda {
+        binding: Gc<'gc, List<'gc, UniqueName<'gc>>>,
+        body: Closure<'gc>,
+    },
+    Invalid,
+}
+
+#[derive(Clone, Collect)]
+#[collect(no_drop)]
+pub enum List<'gc, T> {
+    Cons(T, Gc<'gc, Self>),
+    Nil,
+}
+
+impl<'gc, T: Collect<'gc>> List<'gc, T> {
+    pub fn nil(mc: &Mutation<'gc>) -> Gc<'gc, Self> {
+        Gc::new(mc, Self::Nil)
+    }
+    pub fn cons(mc: &Mutation<'gc>, head: T, tail: Gc<'gc, Self>) -> Gc<'gc, Self> {
+        Gc::new(mc, Self::Cons(head, tail))
+    }
+    pub fn is_empty(&self) -> bool {
+        matches!(self, Self::Nil)
+    }
+    pub fn head(&self) -> Option<&T> {
+        if let Self::Cons(hd, _) = self {
+            Some(hd)
+        } else {
+            None
+        }
+    }
+    pub fn tail(xs: Gc<'gc, Self>) -> Gc<'gc, Self> {
+        if let Self::Cons(_, tail) = *xs {
+            tail
+        } else {
+            xs
+        }
+    }
+}
+
+struct ListIter<'gc, T>(Gc<'gc, List<'gc, T>>);
+impl<'gc, T> Iterator for ListIter<'gc, T> {
+    type Item = &'gc T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let list = self.0;
+        match list.as_ref() {
+            List::Cons(head, tail) => {
+                self.0 = *tail;
+                Some(head)
+            }
+            List::Nil => None,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct EvalContext<'gc> {
+    bindings: Map<UniqueName<'gc>, ValuePtr<'gc>>,
+}
+
+impl<'gc> EvalContext<'gc> {
+    fn with(&self, name: UniqueName<'gc>, value: ValuePtr<'gc>) -> Self {
+        let bindings = self.bindings.insert(name, value);
+        Self { bindings }
+    }
+}
+
+unsafe impl<'gc> Collect<'gc> for EvalContext<'gc> {
+    const NEEDS_TRACE: bool = true;
+
+    fn trace<T: gc_arena::collect::Trace<'gc>>(&self, cc: &mut T) {
+        for (k, v) in self.bindings.iter() {
+            k.trace(cc);
+            v.trace(cc);
+        }
+    }
+}
+
+fn evaluate<'gc>(mc: &Mutation<'gc>, ctx: EvalContext<'gc>, term: TermPtr<'gc>) -> ValuePtr<'gc> {
+    match &**term {
+        Term::Var(name) => ctx
+            .bindings
+            .get(name)
+            .copied()
+            .unwrap_or_else(|| Gc::new(mc, WithSpan(Value::Invalid, term.1))),
+        Term::Universe => Gc::new(mc, WithSpan(Value::Universe, term.1)),
+        Term::Let {
+            name,
+            binding,
+            body,
+        } => {
+            let bound = evaluate(mc, ctx.clone(), *binding);
+            let ctx = ctx.with(*name, bound);
+            evaluate(mc, ctx, *body)
+        }
+        Term::Lambda { binding, body } => Gc::new(
+            mc,
+            WithSpan(
+                Value::Lambda {
+                    binding: *binding,
+                    body: Closure {
+                        variables: ctx.bindings,
+                        body: *body,
+                    },
+                },
+                term.1,
+            ),
+        ),
+        Term::Pi { name, arg, body } => Gc::new(
+            mc,
+            WithSpan(
+                Value::Pi {
+                    name: name.clone(),
+                    arg: evaluate(mc, ctx.clone(), *arg),
+                    body: Closure {
+                        variables: ctx.bindings,
+                        body: *body,
+                    },
+                },
+                term.1,
+            ),
+        ),
+        Term::ClosureCall(lam, arg) => {
+            let lam = evaluate(mc, ctx.clone(), *lam);
+            let arg = evaluate(mc, ctx.clone(), *arg);
+            match &**lam {
+                Value::Lambda { binding, body } => {
+                    let variables = body.variables.insert(binding.head().cloned().unwrap(), arg);
+                    let remaining = List::tail(*binding);
+
+                    if remaining.is_empty() {
+                        evaluate(
+                            mc,
+                            EvalContext {
+                                bindings: variables,
+                            },
+                            body.body,
+                        )
+                    } else {
+                        Gc::new(
+                            mc,
+                            WithSpan(
+                                Value::Lambda {
+                                    binding: remaining,
+                                    body: Closure {
+                                        variables,
+                                        body: body.body,
+                                    },
+                                },
+                                term.1,
+                            ),
+                        )
+                    }
+                }
+                _ => Gc::new(mc, WithSpan(Value::ClosureCall(lam, arg), term.1)),
+            }
+        }
+        _ => Gc::new(mc, WithSpan(Value::Stuck(term), term.1)),
+    }
+}
+
+fn quote<'gc>(mc: &Mutation<'gc>, value: ValuePtr<'gc>) -> TermPtr<'gc> {
+    match &**value {
+        Value::Stuck(term) => *term,
+        Value::Var(unique_name) => Gc::new(mc, WithSpan(Term::Var(unique_name.clone()), value.1)),
+        Value::ClosureCall(lam, arg) => {
+            let lam = quote(mc, *lam);
+            let arg = quote(mc, *arg);
+            Gc::new(mc, WithSpan(Term::ClosureCall(lam, arg), value.1))
+        }
+        Value::Universe => Gc::new(mc, WithSpan(Term::Universe, value.1)),
+        Value::Pi { name, arg, body } => {
+            let arg = quote(mc, *arg);
+            let var = Gc::new(mc, WithSpan(Value::Var(name.clone()), arg.1));
+            let env = EvalContext {
+                bindings: body.variables.clone(),
+            }
+            .with(name.clone(), var);
+            let body = quote(mc, evaluate(mc, env, body.body));
+            Gc::new(
+                mc,
+                WithSpan(
+                    Term::Pi {
+                        name: name.clone(),
+                        arg,
+                        body,
+                    },
+                    value.1,
+                ),
+            )
+        }
+        Value::Lambda { binding, body } => {
+            let env = EvalContext {
+                bindings: body.variables.clone(),
+            };
+            let span = value.1;
+            let env = ListIter(*binding).fold(env, |acc: EvalContext, var| {
+                let value = Gc::new(mc, WithSpan(Value::Var(var.clone()), span));
+                acc.with(var.clone(), value)
+            });
+            let body = quote(mc, evaluate(mc, env, body.body));
+            Gc::new(
+                mc,
+                WithSpan(
+                    Term::Lambda {
+                        binding: *binding,
+                        body,
+                    },
+                    value.1,
+                ),
+            )
+        }
+        Value::Invalid => Gc::new(mc, WithSpan(Term::Invalid, value.1)),
+    }
+}
